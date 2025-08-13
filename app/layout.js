d01388e34f135921b3636e5211737548ddd8aa1e
Clifford <c.iyac123@gmail.com>
2025-08-13 14:32:09 +0800

    update

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,16 @@ import "./globals.css";
 export const metadata = {
   title: "Fordy Portfolio",
   description: "This is my Portfolio site",
+  alternates: {
+    canonical: "https://fordy-next.vercel.app/",
+  },
   openGraph: {
     title: "Fordy Portfolio",
     description: "This is my Portfolio site",
     url: "https://fordy-next.vercel.app/",
     images: [
       {
-        url: "https://fordy-next.vercel.app/",
+        url: "https://fordy-next.vercel.app/preview-image.jpg",
         alt: "Fordy Portfolio Preview",
       },
     ],
@@ -20,7 +23,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Fordy Portfolio",
     description: "This is my Portfolio site",
-    images: ["https://fordy-next.vercel.app/"],
+    images: ["https://fordy-next.vercel.app/preview-image.jpg"],
   },
 };
 
